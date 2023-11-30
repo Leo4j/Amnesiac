7347260fa8bbacf5c8e95ffd8c6b728af8149637
Rob <61951374+Leo4j@users.noreply.github.com>
2023-11-30 19:36:39 +0000

    Update Invoke-SessionHunter.ps1

--- Tools/Invoke-SessionHunter.ps1
+++ Tools/Invoke-SessionHunter.ps1
@@ -81,28 +81,23 @@ function Invoke-SessionHunter {
 	if($UserName -AND $Password){$CheckAsAdmin = $True}
 	
 	if(!$Domain){
-		try{
-			$RetrieveDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-			$Domain = $RetrieveDomain.Name
-			$ldapretrieveddomain = $True
-		}
-		catch{$Domain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select Domain | Format-Table -HideTableHeaders | out-string | ForEach-Object { $_.Trim() }}
+		$Domain = $env:USERDNSDOMAIN
+		if(!$Domain){$Domain = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.Trim()}
+		if(!$Domain){$Domain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select Domain | Format-Table -HideTableHeaders | out-string | ForEach-Object { $_.Trim() }}
 	}
 
- 	if(!$DomainController){
-		if($ldapretrieveddomain){
-			$dcObjects = @($RetrieveDomain.DomainControllers)
-			$DomainController = $dcObjects[0].Name
-			#$DomainController = ($RetrieveDomain.FindDomainController()).Name
-			#$DomainController = $allDCs[0].Name
+	if(!$DomainController){
+		$currentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain((New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $Domain)))
+		$domainControllers = $currentDomain.DomainControllers
+		$DomainController = $domainControllers[0].Name
+		if(!$DomainController){
+			$DomainController = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().RidRoleOwner.Name
 		}
-		else{
+		if(!$DomainController){
 			$result = nslookup -type=all "_ldap._tcp.dc._msdcs.$Domain" 2>$null
 			$DomainController = ($result | Where-Object { $_ -like '*svr hostname*' } | Select-Object -First 1).Split('=')[-1].Trim()
 		}
 	}
-	
-	$connection = Establish-LDAPSession -Domain $Domain -DomainController $DomainController
 	
 	if($Targets){
 		
@@ -120,19 +115,33 @@ function Invoke-SessionHunter {
 		}
 	}
 	
-	elseif($Servers){
-		$Computers = Get-ADServers -ADCompDomain $Domain -LdapConnection $connection
-		$Computers = $Computers | Sort-Object
-	}
-
-	elseif($Workstations){
-		$Computers = Get-ADWorkstations -ADCompDomain $Domain -LdapConnection $connection
-		$Computers = $Computers | Sort-Object
-	}
-	
 	else{
-		$Computers = Get-ADComputers -ADCompDomain $Domain -LdapConnection $connection
-		$Computers = $Computers | Sort-Object
+		$Computers = @()
+		$objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+		if($Domain){
+			if($DomainController){
+				$TempDomainName = "DC=" + $Domain.Split(".")
+				$domainDN = $TempDomainName -replace " ", ",DC="
+				$ldapPath = "LDAP://$DomainController/$domainDN"
+				$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+			}
+			else{$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$Domain")}
+		}
+		else{$objSearcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry}
+		if($Servers){
+			$objSearcher.Filter = "(&(objectCategory=computer)(objectClass=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(operatingSystem=*Server*))"
+		}
+
+		elseif($Workstations){
+			$objSearcher.Filter = "(&(objectCategory=computer)(objectClass=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(!(operatingSystem=*Server*)))"
+		}
+		
+		else{
+			$objSearcher.Filter = "(&(sAMAccountType=805306369)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
+		}
+		$objSearcher.PageSize = 1000
+		$Computers = $objSearcher.FindAll() | ForEach-Object { $_.properties.dnshostname }
+		$Computers = $Computers | Sort-Object -Unique
 	}
 	
 	if(!$IncludeLocalHost){
@@ -573,8 +582,8 @@ function Invoke-SessionHunter {
 		$username = ($result.UserSession -split '\\')[1]
 		$TargetHost = $result.HostName
 		if($username -like '*$'){$result.AdmCount = "N/A"}
-		else{$result.AdmCount = AdminCount -UserName $username -LdapConnection $connection -Domain $Domain}
-		$result.OperatingSystem = Get-OS -HostName $TargetHost -LdapConnection $connection -Domain $Domain
+		else{$result.AdmCount = AdminCount -UserName $username -Domain $Domain}
+		$result.OperatingSystem = Get-OS -HostName $TargetHost -Domain $Domain
 	}
 	
  	# Show Results
@@ -749,229 +758,37 @@ function Invoke-WMIRemoting {
 }
 '@
 
-function Establish-LDAPSession {
-    param (
-        [string]$Domain,
-        [string]$DomainController
-    )
-
-    $ErrorActionPreference = "SilentlyContinue"
-
-    # If the DomainController parameter is just a name (not FQDN), append the domain to it.
-    if ($DomainController -notlike "*.*") {
-        $DomainController = "$DomainController.$Domain"
-    }
-
-    # Define LDAP parameters
-    $ldapServer = $DomainController
-    $ldapPort = 389 # Use 636 for LDAPS (SSL)
-
-    # Load necessary assembly
-    Add-Type -AssemblyName "System.DirectoryServices.Protocols"
-
-    # Create LDAP directory identifier
-    $identifier = New-Object System.DirectoryServices.Protocols.LdapDirectoryIdentifier($ldapServer, $ldapPort)
-
-    # Establish LDAP connection as current user
-    $ldapConnection = New-Object System.DirectoryServices.Protocols.LdapConnection($identifier)
-
-    # Use Negotiate (Kerberos or NTLM) for authentication
-    $ldapConnection.AuthType = [System.DirectoryServices.Protocols.AuthType]::Negotiate
-
-    # Bind (establish connection)
-    $ldapConnection.Bind()  # Bind as the current user
-
-    return $ldapConnection
-}
-
-function Get-ADComputers {
-    param (
-        [string]$ADCompDomain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection  # The previously established connection
-    )
-
-    $ErrorActionPreference = "SilentlyContinue"
-
-    # Construct distinguished name for the domain.
-    $domainDistinguishedName = "DC=" + ($ADCompDomain -replace "\.", ",DC=")
-
-    # Set up an LDAP search request.
-    $ldapFilter = "(&(objectCategory=computer)(objectClass=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
-    $attributesToLoad = @("dNSHostName")
-
-    $allcomputers = @()
-
-    # Create a page request control
-    $pageRequest = New-Object System.DirectoryServices.Protocols.PageResultRequestControl(1000)
-    
-    do {
-        $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-            $domainDistinguishedName,     # Base DN
-            $ldapFilter,                  # LDAP filter
-            [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-            $attributesToLoad             # Attributes to retrieve
-        )
-
-        # Add the page request control to the search request.
-        $searchRequest.Controls.Add($pageRequest)
-        
-        # Perform the search using the provided LdapConnection.
-        $searchResponse = $LdapConnection.SendRequest($searchRequest)
-
-        # Check for a page response control and update the cookie for the next request.
-        $pageResponse = $searchResponse.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
-
-        if ($pageResponse) {
-            $pageRequest.Cookie = $pageResponse.Cookie
-        }
-
-        # Parse the results.
-        foreach ($entry in $searchResponse.Entries) {
-            $allcomputers += $entry.Attributes["dNSHostName"][0]
-        }
-
-    } while ($pageRequest.Cookie.Length -ne 0)
-
-    return $allcomputers
-}
-
-function Get-ADWorkstations {
-    param (
-        [string]$ADCompDomain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection  # The previously established connection
-    )
-
-    $ErrorActionPreference = "SilentlyContinue"
-
-    # Construct distinguished name for the domain.
-    $domainDistinguishedName = "DC=" + ($ADCompDomain -replace "\.", ",DC=")
-
-    # Set up an LDAP search request.
-    $ldapFilter = "(&(objectCategory=computer)(objectClass=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(!(operatingSystem=*Server*)))"
-    $attributesToLoad = @("dNSHostName")
-
-    $allcomputers = @()
-
-    # Create a page request control
-    $pageRequest = New-Object System.DirectoryServices.Protocols.PageResultRequestControl(1000)
-    
-    do {
-        $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-            $domainDistinguishedName,     # Base DN
-            $ldapFilter,                  # LDAP filter
-            [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-            $attributesToLoad             # Attributes to retrieve
-        )
-
-        # Add the page request control to the search request.
-        $searchRequest.Controls.Add($pageRequest)
-        
-        # Perform the search using the provided LdapConnection.
-        $searchResponse = $LdapConnection.SendRequest($searchRequest)
-
-        # Check for a page response control and update the cookie for the next request.
-        $pageResponse = $searchResponse.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
-
-        if ($pageResponse) {
-            $pageRequest.Cookie = $pageResponse.Cookie
-        }
-
-        # Parse the results.
-        foreach ($entry in $searchResponse.Entries) {
-            $allcomputers += $entry.Attributes["dNSHostName"][0]
-        }
-
-    } while ($pageRequest.Cookie.Length -ne 0)
-
-    return $allcomputers
-}
-
-function Get-ADServers {
-    param (
-        [string]$ADCompDomain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection  # The previously established connection
-    )
-
-    $ErrorActionPreference = "SilentlyContinue"
-
-    # Construct distinguished name for the domain.
-    $domainDistinguishedName = "DC=" + ($ADCompDomain -replace "\.", ",DC=")
-
-    # Set up an LDAP search request.
-    $ldapFilter = "(&(objectCategory=computer)(objectClass=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(operatingSystem=*Server*))"
-    $attributesToLoad = @("dNSHostName")
-
-    $allcomputers = @()
-
-    # Create a page request control
-    $pageRequest = New-Object System.DirectoryServices.Protocols.PageResultRequestControl(1000)
-    
-    do {
-        $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-            $domainDistinguishedName,     # Base DN
-            $ldapFilter,                  # LDAP filter
-            [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-            $attributesToLoad             # Attributes to retrieve
-        )
-
-        # Add the page request control to the search request.
-        $searchRequest.Controls.Add($pageRequest)
-        
-        # Perform the search using the provided LdapConnection.
-        $searchResponse = $LdapConnection.SendRequest($searchRequest)
-
-        # Check for a page response control and update the cookie for the next request.
-        $pageResponse = $searchResponse.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
-
-        if ($pageResponse) {
-            $pageRequest.Cookie = $pageResponse.Cookie
-        }
-
-        # Parse the results.
-        foreach ($entry in $searchResponse.Entries) {
-            $allcomputers += $entry.Attributes["dNSHostName"][0]
-        }
-
-    } while ($pageRequest.Cookie.Length -ne 0)
-
-    return $allcomputers
-}
-
 function AdminCount {
     param (
         [string]$UserName,
-		[string]$Domain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection
+        [string]$Domain
     )
 
     $ErrorActionPreference = "SilentlyContinue"
 
-    # Assuming all user objects are under the default "CN=Users" container.
-    # Adjust this according to your Active Directory structure.
+    # Construct distinguished name for the domain.
     $domainDistinguishedName = "DC=" + ($Domain -replace "\.", ",DC=")
-    $baseDN = "CN=Users," + $domainDistinguishedName  # Replace with your domain DN
+    $baseDN = "CN=Users,$domainDistinguishedName"  # Replace with your domain DN
 
     # Set up the search filter
     $ldapFilter = "(sAMAccountName=$UserName)"
     $attributesToLoad = @("adminCount")
 
-    $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-        $baseDN,            # Base DN
-        $ldapFilter,        # LDAP filter
-        [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-        $attributesToLoad   # Attributes to retrieve
-    )
+    # Create the directory searcher
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher
+    $searcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$baseDN")
+    $searcher.Filter = $ldapFilter
+    $searcher.PropertiesToLoad.Add($attributesToLoad) > $null
 
-    # Perform the search using the LdapConnection.
-    $searchResponse = $LdapConnection.SendRequest($searchRequest)
+    # Perform the search
+    $result = $searcher.FindOne()
 
     # Check if results were returned and output the adminCount property.
-    if ($searchResponse.Entries -ne $null -and $searchResponse.Entries.Count -ne 0) {
-        $entry = $searchResponse.Entries[0]
-        if ($entry.Attributes["adminCount"] -ne $null -and $entry.Attributes["adminCount"].Count -gt 0) {
-            $adminCount = $entry.Attributes["adminCount"][0]
-            return ($adminCount -eq 1)
-		} else {
+    if ($result -ne $null) {
+        $entry = $result.GetDirectoryEntry()
+        if ($entry.Properties["adminCount"].Value -ne $null) {
+            return ($entry.Properties["adminCount"].Value -eq 1)
+        } else {
             return $false
         }
     } else {
@@ -982,34 +799,35 @@ function AdminCount {
 function Get-OS {
     param (
         [string]$HostName,
-		[string]$Domain,
-        [System.DirectoryServices.Protocols.LdapConnection]$LdapConnection
+        [string]$Domain
     )
 
     $ErrorActionPreference = "SilentlyContinue"
 
-    # Construct the search base. Assuming all computer objects are under the default "CN=Computers" container.
-    # You might need to adjust this if your AD structure differs.
-	$baseDN = "DC=" + ($Domain -replace "\.", ",DC=")
+    # Construct the search base.
+    $baseDN = "DC=" + ($Domain -replace "\.", ",DC=")
 
     $ldapFilter = "(&(objectCategory=computer)(name=$HostName))"
-    $attributesToLoad = @("operatingSystem")
+    $attributesToLoad = "operatingSystem"
 
-    $searchRequest = New-Object System.DirectoryServices.Protocols.SearchRequest(
-        $baseDN,            # Base DN
-        $ldapFilter,        # LDAP filter
-        [System.DirectoryServices.Protocols.SearchScope]::Subtree,
-        $attributesToLoad   # Attributes to retrieve
-    )
+    # Create the directory searcher
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher
+    $searcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$baseDN")
+    $searcher.Filter = $ldapFilter
+    $searcher.PropertiesToLoad.Add($attributesToLoad) > $null
 
-    # Perform the search using the LdapConnection.
-    $searchResponse = $LdapConnection.SendRequest($searchRequest)
+    # Perform the search
+    $result = $searcher.FindOne()
 
     # Check if results were returned and output the operatingSystem property.
-    if ($searchResponse.Entries.Count -eq 0) {
-        return $null
+    if ($result -ne $null) {
+        $entry = $result.GetDirectoryEntry()
+        if ($entry.Properties["operatingSystem"].Value -ne $null) {
+            return $entry.Properties["operatingSystem"].Value.ToString()
+        } else {
+            return $null
+        }
     } else {
-        # The following line retrieves the 'operatingSystem' attribute from the search result.
-        return $searchResponse.Entries[0].Attributes["operatingSystem"][0].ToString()
+        return $null
     }
 }
