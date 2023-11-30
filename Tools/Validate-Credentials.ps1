23228a342944e7c2d6beb56c97619230d5cea96c
Rob <61951374+Leo4j@users.noreply.github.com>
2023-11-30 17:45:16 +0000

    Update Validate-Credentials.ps1

--- Tools/Validate-Credentials.ps1
+++ Tools/Validate-Credentials.ps1
@@ -1,38 +1,21 @@
-Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-
-function Validate-Credentials{
-	Param
-    (
-		[string]
-        $UserName,
-		[string]
-        $Password,
-		[string]
-        $Domain,
-		[string]
-        $DomainController
+function Validate-Credentials {
+    param(
+        [string]$UserName,
+        [string]$Password,
+        [string]$Domain
     )
-	
-	if(!$Domain){
-	    $Domain = $env:USERDNSDOMAIN
-	    if(!$Domain){$Domain = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.Trim()}
-	    if(!$Domain){$Domain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select Domain | Format-Table -HideTableHeaders | out-string | ForEach-Object { $_.Trim() }}
-	}
-	
-	if(!$DomainController){
-	    $currentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain((New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $Domain)))
-	    $domainControllers = $currentDomain.DomainControllers
-	 	$DomainController = $domainControllers[0].Name
-	  	if(!$DomainController){
-	        	$DomainController = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().RidRoleOwner.Name
-	    	}
-	  	if(!$DomainController){
-	        	$result = nslookup -type=all "_ldap._tcp.dc._msdcs.$Domain" 2>$null
-	        	$DomainController = ($result | Where-Object { $_ -like '*svr hostname*' } | Select-Object -First 1).Split('=')[-1].Trim()
-	  	}
-	}
 
-	$principalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain, $Domain, $DomainController)
-	if($principalContext.ValidateCredentials($UserName, $Password)){"[+] Credentials Validation: Success"}
-	else{"[-] Credentials Validation: Failed"}
+    $LDAPPath = "LDAP://"
+    $LDAPPath += $Domain
+
+    try {
+        $directoryEntry = New-Object System.DirectoryServices.DirectoryEntry($LDAPPath, $UserName, $Password)
+        if ($directoryEntry.name -ne $null) {
+            Write-Output "[+] Authentication Successful for user $UserName"
+        } else {
+            Write-Output "[-] Authentication Failed for user $UserName"
+        }
+    } catch {
+        Write-Output "[-] Error occurred during authentication for user $UserName : $_"
+    }
 }
